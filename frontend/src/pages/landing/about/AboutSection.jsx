76c7242bd0a187f9a786ee31a5a449506edfefa4
Eldrich-Yurii <eldrichjyuris@gmail.com>
2025-01-30 17:28:22 +0800

    edit about section

--- frontend/src/pages/landing/about/AboutSection.jsx
+++ frontend/src/pages/landing/about/AboutSection.jsx
@@ -1,23 +1,23 @@
-import Sanjuan from '../../../assets/about-img.jpg'
+import Sanjuan from '../../../assets/SANJUAN.jpg'
 
 export default function About() {
     return (
         // grid form na template two columns
 
-        <div className='grid grid-cols-2 lg:px-[4.5rem] lg:pt-48'>
+        <div className='flex justify-between gap-4 lg:px-[4.5rem] lg:pt-48'>
             
             {/* container ng image ng san juan */}
-            <section>   
-                <img className='rounded-lg' src={Sanjuan} alt="sanjuan-munisipyo" width={550}/>
+            <section className='w-[39rem]'>   
+                <img  className='rounded-lg' src={Sanjuan} alt="sanjuan-munisipyo"/>
             </section>
 
             {/* container ng title at description */}
-            <section className='pt-[65px] pl-6 font-semibold'>
+            <section className='pl-4 font-semibold w-[40rem]'>
                 <div>
-                    <h2 className='font-extrabold text-red-700 pb-2 text-[48px]'>ABOUT</h2>
+                    <h2 className='font-extrabold text-red-700 text-3xl pb-2'>ABOUT</h2>
                 </div>
                 <div>
-                    <p className='leading-[180%] text-[20px] font-semibold'>The City Environment and Natural Resources Office (CENRO) supports the Sangguniang Panlungsod and the city mayor in ensuring the delivery of environmental services and facilities, 
+                    <p className='leading-[160%] text-[20px] font-semibold'>The City Environment and Natural Resources Office (CENRO) supports the Sangguniang Panlungsod and the city mayor in ensuring the delivery of environmental services and facilities, 
                         as mandated by the Local Government Code under section 17 of 1991 and Republic Act No. 9388. 
                         CENRO develops and implements environment-related programs and projects, and coordinates with government and non-government organizations to prevent and control land, air, 
                         and water pollution. 
