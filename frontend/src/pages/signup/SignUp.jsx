e2c700172fcd19f3354793e8d0f87af91350b305
Golbez020 <ulysisavila20@gmail.com>
2025-02-04 00:38:05 +0800

    edited the sign up page

--- frontend/src/pages/signup/SignUp.jsx
+++ frontend/src/pages/signup/SignUp.jsx
@@ -17,20 +17,20 @@ export default function SignUp() {
         width={180}
       />
       <div className="grid grid-cols-2 h-screen">
-        <section className="login-pic w-full brightness-75 contrast-125"></section>
+      <section className="login-pic w-full brightness-75 contrast-125"></section>
         <section className="flex flex-col justify-center items-center px-2">
           {/* Create an Account Form */}
-          <div className="w-[34rem] h-[34rem] py-12 overflow-y-auto scrollbar">
+          <div className="w-[32rem] h-[34rem] py-12 overflow-y-auto border-2 border-stone-400 rounded-xl outline-offset-4 bg-stone-300/50 shadow-2xl" >
             <div className="w-[25rem] pb-8 text-blue-900">
-              <h2 className="font-black text-5xl pb-2 leading-[100%]">
+              <h2 className="pl-12 font-black text-5xl pb-2 leading-[100%]">
                 CREATE AN ACCOUNT
               </h2>
-              <p className="text-lg font-medium w-56 leading-[120%]">
+              <p className="pl-12 text-lg font-medium w-80 leading-[120%]">
                 Please fill out the form to create an account.
               </p>
             </div>
             {/* Full Name */}
-            <div className=" grid grid-cols-3 gap-2">
+            <div className=" pl-12 pr-12 grid grid-cols-3 gap-2">
               <div className="pb-2 flex flex-col gap-2">
                 <label className="font-semibold text-blue-900" htmlFor="fName">
                   First Name
@@ -72,7 +72,7 @@ export default function SignUp() {
               </div>
             </div>
             {/* Birth Date */}
-            <div className="pb-3 flex flex-col gap-2">
+            <div className=" pl-12 pr-12 pb-3 flex flex-col gap-2">
               <label className="font-semibold text-blue-900" htmlFor="bdate">
                 Birth Date
               </label>
@@ -85,10 +85,10 @@ export default function SignUp() {
               />
             </div>
             {/* Address */}
-            <label className="font-semibold text-blue-900" htmlFor="">
+            <label className="pl-12 pr-12 font-semibold text-blue-900" htmlFor="">
               Address
             </label>
-            <div className="pt-2 grid grid-cols-3 gap-2">
+            <div className=" pl-12 pr-12 pt-2 grid grid-cols-3 gap-2">
               <div className="pb-2">
                 <input
                   type="text"
@@ -99,7 +99,7 @@ export default function SignUp() {
                   className="border-blue-900 border-2 w-full h-12 rounded-lg pl-2"
                 />
               </div>
-              <div>
+              <div >
                 {/* <label className="font-semibold text-blue-900" htmlFor="mName">
                   Middle Name
                 </label> */}
@@ -122,10 +122,10 @@ export default function SignUp() {
                   name="brgy"
                   placeholder="Barangay"
                   required
-                  className="border-blue-900 border-2 w-full h-12 rounded-lg pl-2"
+                  className=" border-blue-900 border-2 w-full h-12 rounded-lg pl-2"
                 />
               </div>
-            </div>
+            
             <input
               type="text"
               id="city"
@@ -134,8 +134,9 @@ export default function SignUp() {
               required
               className="border-blue-900 border-2 w-full h-12 rounded-lg pl-2"
             />
+            </div>
             {/* Account credentials */}
-            <div className="pt-3">
+            <div className="pt-3 pl-12 pr-12">
               <div className="grid grid-cols-2 gap-2">
                 <div className="flex flex-col gap-2">
                   <label
@@ -184,16 +185,16 @@ export default function SignUp() {
                 />
               </div>
             </div>
-            <div>
-              <button className="bg-blue-900 text-white w-full h-12 font-bold text-xl rounded-lg">
+            <div className="pl-12 pr-12">
+              <button className="bg-blue-900 text-white w-full h-12 font-bold text-xl rounded-lg hover:bg-blue-500">
                 Create an Account
               </button>
             </div>
             <div>
               <p className="text-lg font-semibold text-center pt-8">
-                Already have an account?
+                Already have an account? 
                 <Link to="/login" className="underline text-blue-900">
-                  Login Now
+                   Login Now
                 </Link>
               </p>
             </div>
