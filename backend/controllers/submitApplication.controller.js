7fafef5f25bcf73a728979f7f8cf8c118b43324a
Eldrich-Yurii <eldrichjyuris@gmail.com>
2025-02-21 21:39:33 +0800

    added applicationSchema and submit application controller

---
+++ backend/controllers/submitApplication.controller.js
@@ -0,0 +1,11 @@
+import applicationSchema from "../models/applicationSchema";
+
+const submitApplication = async (req, res) => {
+    try {
+        const { userId, formType } = req.body
+
+        
+    } catch(err) {
+
+    }
+}
---
+++ backend/models/applicationSchema.js
@@ -0,0 +1,32 @@
+import mongoose from "mongoose";
+
+const applicationSchema = new mongoose.Schema(
+   {
+     userId: {
+       type: mongoose.Schema.Types.ObjectId,
+       ref: "user", // Reference to the normal user's account
+       required: true,
+     },
+     formType: {
+       type: String,
+       enum: ["New Business Application", "Renewal of Business"], // Only allow these options
+       required: true,
+     },
+     status: {
+       type: String,
+       enum: ["Pending", "Approved", "Rejected"],
+       default: "Pending", // Default status when the form is submitted
+     },
+     pdfPath: {
+       type: String, // Store the file path where the PDF is generated
+       required: true,
+     },
+     submittedAt: {
+       type: Date,
+       default: Date.now, // Automatically sets the submission timestamp
+     },
+   },
+   { timestamps: true } // Automatically add createdAt & updatedAt
+ );
+ 
+ export default mongoose.model("Application", applicationSchema);
