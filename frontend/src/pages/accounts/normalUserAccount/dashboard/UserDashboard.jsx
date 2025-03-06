422919c34371382a9c66a59ba54d90355a35edfd
Golbez020 <ulysisavila20@gmail.com>
2025-03-06 14:20:49 +0800

    Added description for user dashboard

--- frontend/src/pages/accounts/normalUserAccount/dashboard/UserDashboard.jsx
+++ frontend/src/pages/accounts/normalUserAccount/dashboard/UserDashboard.jsx
@@ -1,10 +1,46 @@
-
+import {
+  Button,
+  Card,
+  CardBody,
+  CardFooter,
+  CardHeader,
+  Typography,
+} from "@material-tailwind/react";
+import { getUserApplication, uploadAssessment } from "../../../../api/ApplicationApi";
+import { useEffect, useState } from "react";
+import SubmitApplication from "../../../../components/modal/SubmitApplication"
 
 export default function UserDashboard() {
   return (
-    <div className="h-screen bg-pink-200">
-      User Dashboard
-    </div>
+    <div className="h-screen">
+    <Card className="h-[32rem] w-full px-6 shadow-lg">
+      <CardHeader className="rounded-none" floated={false} shadow={false}>
+        <div className=" flex justify-between">
+          <section>
+            <Typography
+              variant="h2"
+              className="text-blue-800 font-extrabold font-inter"
+            >
+              My Dashboard - WORK IN PROGRESS
+            </Typography>
+            <p className="w-64 text-sm leading-[120%] py-2 font-semibold text-gray-600 tracking-tight">
+              Progress bar of your application status will show here. 
+            </p>
+          </section>
+          
+        </div>
+      </CardHeader>
+      <br />
+      <CardBody>
+        <table className="w-full min-w-max table-auto text-left">
+         
+        </table>
+       
+      </CardBody>
+      <CardFooter className="flex items-center justify-between border-t border-blue-gray-50 p-4">
+      </CardFooter>
+    </Card>
+  </div>
   )
 }
 
