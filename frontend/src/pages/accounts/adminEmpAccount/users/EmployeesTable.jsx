353baf827c13da68aad59949cc111aecc8602ff0
Golbez020 <ulysisavila20@gmail.com>
2025-03-11 03:29:04 +0800

    removed search and footer buttons

--- frontend/src/pages/accounts/adminEmpAccount/users/EmployeesTable.jsx
+++ frontend/src/pages/accounts/adminEmpAccount/users/EmployeesTable.jsx
@@ -225,17 +225,6 @@ export default function EmployeesTable() {
           )}
         </CardBody>
         <CardFooter className="flex items-center justify-between border-t border-blue-gray-50 p-4">
-          <Typography variant="small" color="blue-gray" className="font-normal">
-            Page 1 of 1
-          </Typography>
-          <div className="flex gap-2">
-            <Button variant="outlined" size="sm" className="text-blue-800">
-              Previous
-            </Button>
-            <Button variant="outlined" size="sm" className="text-blue-800">
-              Next
-            </Button>
-          </div>
         </CardFooter>
       </Card>
     </div>
